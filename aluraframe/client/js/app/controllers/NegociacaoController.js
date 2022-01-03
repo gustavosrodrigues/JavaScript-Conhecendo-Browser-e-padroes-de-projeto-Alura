83d54f032c211f1ecbcf87b7825b936e40205c3c
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2022-01-03 18:18:35 -0300

    Aula 2 - Associando metodos do controller as acoes do usuario

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -3,6 +3,15 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        alert(`Fui apertado`);
+        
+        let $ = document.querySelector.bind(document);
+
+        let data = $('#data');
+        let valor = $('#valor');
+        let quantidade = $('#quantidade');
+
+        console.log(data.value);
+        console.log(valor.value);
+        console.log(quantidade.value);
     }
 }
