aa13355aab80f735e5aed7f011f9d4c024fa4cc2
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2022-01-04 17:16:19 -0300

    Aula 2 - O problema com datas

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -13,8 +13,15 @@ class NegociacaoController {
 
         console.log(this._data.value);
 
-        let date = new Date(this._data.value.split('-')); //recupera a data em string, separa e monta num tipo Date
+        let date = this._data.value.split('-'); //recupera a data em string, separa os valores
+        const dia = date[2];
+        const mes = date[1] - 1; //Diminui o mês em 1, para mostrar o mês correto
+        const ano = date[0];
+
+        let novaData = new Date(ano, mes, dia); // Pega os valores da data e monta num tipo Date
+        
         console.log(date);
+        console.log(novaData);
         /* let negociacao = new Negociacao(
             this._data.value, this._valor.value, this._quantidade.value
         );
