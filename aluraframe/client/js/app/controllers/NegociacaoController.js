498450f555effc666244f29de9e18962f520374a
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2022-01-04 17:29:36 -0300

    Aula 2 - Resolvendo um problema com o paradigma funcional

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -9,19 +9,25 @@ class NegociacaoController {
     }
 
     adiciona(event) {
-        event.preventDefault();
+        event.preventDefault();        
 
-        console.log(this._data.value);
-
-        let date = this._data.value.split('-'); //recupera a data em string, separa os valores
+        /* let date = this._data.value.split('-'); //recupera a data em string, separa os valores
         const dia = date[2];
         const mes = date[1] - 1; //Diminui o mês em 1, para mostrar o mês correto
         const ano = date[0];
 
-        let novaData = new Date(ano, mes, dia); // Pega os valores da data e monta num tipo Date
+        let novaData = new Date(ano, mes, dia); // Pega os valores da data e monta num tipo Date */
+
+        let data = new Date(... // o spread (...) coloca cada item separado em um paramêtro do Date
+            this._data.value
+                .split('-')
+                .map(function(item, indice) {
+                    return item - indice % 2 // convenientemente, somente o indice do mês retorna resto = 1
+                })
+        )
         
-        console.log(date);
-        console.log(novaData);
+        console.log(data);
+
         /* let negociacao = new Negociacao(
             this._data.value, this._valor.value, this._quantidade.value
         );
