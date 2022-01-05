825d3b5f0eccfad1dc1933c60324c5b2d99de7b1
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2022-01-05 16:46:45 -0300

    Aula 3 - Template strings e expressoes regulares

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -5,12 +5,15 @@ class DateHelper {
         throw new Error('Esta classe não pode ser instanciada.');
     }
 
-    static dataParaTexto(texto) {
-        return `${texto.getDate()}/${texto.getMonth() + 1}/${texto.getFullYear()}`
+    static dataParaTexto(data) {
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`
     }
 
-    static textoParaData(data) {
-        return new Date(...data.split('-').map((item, indice) => item - indice % 2));
+    static textoParaData(texto) {
+        if (!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+            throw new Error ('A data precisa estar no formato aaaa-mm-dd.')
+
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 
 }
