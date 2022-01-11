cf0c765767ff02cfb2d3abe7b297478902263125
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2022-01-11 12:05:23 -0300

    Criando a classe MensagemView

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,9 +6,14 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputValor = $('#valor');
         this._inputQuantidade = $('#quantidade')
+
         this._listaNegociacoes = new ListaNegociacoes();
         this._negociacoesView = new NegociacoesView($('#negociacoes__view'));
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
     
     adiciona(event) {
@@ -16,6 +21,8 @@ class NegociacaoController {
         
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = "Negociação bem sucedida!"
+        this._mensagemView.update(this._mensagem);
         console.log(this._listaNegociacoes.negociacoes);
 
         /* let dataCompleta = DateHelper.dataParaTexto(negociacao.data);
